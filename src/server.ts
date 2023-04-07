8a9506de0fc6c65baf1dbbe8f497cd9094b74d67
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-06 21:18:06 -0400

    installed cors and morgan and using both packages in server, also using errorHandler middleware that was created

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,28 @@ dotenv.config();
 
 import env from './utils/validateEnv';
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import errorHandler from './middleware/errorHandler';
+import cors from 'cors';
+import corsOptions from './config/corsOptions';
 import connectDB from './config/database';
 
 const app = express();
 
+app.use(cors(corsOptions));
+
 connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello server');
 });
+
+app.use(errorHandler);
 
 const PORT = env.PORT;
 app.listen(PORT, () => {
