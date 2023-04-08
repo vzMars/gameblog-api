f3953dc35c87110c2525f0a5bfece314df258f2b
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-08 18:05:53 -0400

    installed dependencies and imported and used as needed

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,66 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import env from './utils/validateEnv';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import helmet from 'helmet';
 import morgan from 'morgan';
-import errorHandler from './middleware/errorHandler';
 import cors from 'cors';
+import createHttpError from 'http-errors';
+import env from './utils/validateEnv';
 import corsOptions from './config/corsOptions';
+import passportConfig from './config/passport';
 import connectDB from './config/database';
+import errorHandler from './middleware/errorHandler';
+import authRoutes from './routes/auth';
 
 const app = express();
 
+// Passport config
+passportConfig(passport);
+
+// Enable CORS
 app.use(cors(corsOptions));
 
+// Database Connection
 connectDB();
 
+// Secure api
+app.use(helmet());
+
+// Body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Logging
 app.use(morgan('dev'));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello server');
+// Sessions
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: env.DATABASE_URI }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  next(createHttpError(404, '404 Not Found'));
 });
 
+// Error Handler
 app.use(errorHandler);
 
 const PORT = env.PORT;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
