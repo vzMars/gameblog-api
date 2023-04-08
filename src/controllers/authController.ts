474acfba80c223ee51c8ad94a1b71abd0403b6e0
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-08 17:59:38 -0400

    auth controller with working signup method

---
+++ src/controllers/authController.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import passport from 'passport';
+import User from '../models/User';
+
+export const authStatus = (req: Request, res: Response) => {
+  res.status(200).json('auth status');
+};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json('login');
+};
+
+export const logout = (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json('logout');
+};
+
+interface ISignUpBody {
+  email: string;
+  username: string;
+  password: string;
+}
+
+export const signup = async (
+  req: Request<ParamsDictionary, any, ISignUpBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, username, password } = req.body;
+
+  try {
+    const user = await User.signup(email, username, password);
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      res.status(200).json({
+        user: { id: user.id, email: user.email, username: user.username },
+      });
+    });
+  } catch (err) {
+    next(err);
+  }
+};
