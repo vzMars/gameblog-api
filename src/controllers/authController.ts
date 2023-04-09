9337de82f7c473c74b22b7771752aa3d1be8b01b
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-08 22:30:31 -0400

    login route that uses passport.authenticate function and returns logged in user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { ParamsDictionary } from 'express-serve-static-core';
+import createHttpError = require('http-errors');
 import passport from 'passport';
 import User from '../models/User';
 
@@ -13,8 +14,43 @@ export const authStatus = (req: Request, res: Response) => {
   });
 };
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json('login');
+interface ILoginBody {
+  email: string;
+  password: string;
+}
+
+export const login = (
+  req: Request<ParamsDictionary, any, ILoginBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      throw createHttpError(400, 'Please complete all required fields.');
+    }
+
+    passport.authenticate(
+      'local',
+      (err: unknown, user: Express.User, info: unknown) => {
+        if (err) return next(err);
+
+        if (!user) {
+          throw createHttpError(401, 'Incorrect email or password.');
+        }
+
+        req.logIn(user, (err) => {
+          if (err) return next(err);
+          res.status(200).json({
+            user: { id: user.id, email: user.email, username: user.username },
+          });
+        });
+      }
+    )(req, res, next);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const logout = (req: Request, res: Response, next: NextFunction) => {
