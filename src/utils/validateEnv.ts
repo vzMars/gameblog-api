5d46859bcc503bc65cde0a433ca34e0a757af3c5
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-13 10:11:47 -0400

    cloudinary global config added

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,6 +4,9 @@ const env = cleanEnv(process.env, {
   DATABASE_URI: str(),
   PORT: port(),
   SESSION_SECRET: str(),
+  CLOUD_NAME: str(),
+  API_KEY: str(),
+  API_SECRET: str(),
 });
 
 export default env;
